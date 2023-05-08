fcf3cefe967248a5b04aafc24328700001741044
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2023-05-08 22:06:07 +0600

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -123,7 +123,7 @@ import {
       company_name: "BDTask",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "Octobar 2022 - December 2022",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
