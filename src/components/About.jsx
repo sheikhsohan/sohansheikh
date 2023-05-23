9d9734b02ec9ed1ffd713e7c8589f030f7e5482a
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2023-05-23 15:18:54 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Tilt } from 'react-tilt';
-import { motion } from 'framer-motion';
+import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
 
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
-import { SectionWrapper } from '../hoc';
+import { SectionWrapper } from '../hoc'
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -15,6 +15,7 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
+
         <div
           options={{
             max: 45,
@@ -24,40 +25,33 @@ const ServiceCard = ({ index, title, icon }) => {
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
           <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+
           <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+
         </div>
+
       </motion.div>
     </Tilt>
-  );
-};
+  )
+}
 
 const About = () => {
-  const [alertShow, setAlertShow] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      document.title = alertShow
-        ? 'Welcome to My Official Website'
-        : 'Sohan | Web Designer and Developer';
-
-      setAlertShow((prevAlertShow) => !prevAlertShow);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
       <motion.div>
         <p className={styles.heroSubText}>Introduction</p>
-        <h2 className={styles.heroHeadText}>Overview</h2>
+        <h2 className={styles.heroHeadText}>Ovrview</h2>
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
+        variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
@@ -66,7 +60,7 @@ const About = () => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, 'about')
