1daf07afa2542b610f14cd538a930d8b608c7d57
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2023-05-23 17:58:44 +0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,20 @@ import { ComputersCanvas } from "./canvas";
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 const Hero = () => {
+  const forceDesktopMode = () => {
+    const metaViewport = document.querySelector("meta[name='viewport']");
+    if (metaViewport) {
+      metaViewport.setAttribute(
+        "content",
+        "width=1024, initial-scale=1.0, maximum-scale=1.0"
+      );
+    }
+  };
+
+  if (isMobile) {
+    forceDesktopMode();
+  }
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -26,33 +40,27 @@ const Hero = () => {
         </div>
       </div>
 
-      {!isMobile && <ComputersCanvas />}
+      <ComputersCanvas />
 
-      {isMobile && (
-        <div className='absolute inset-0 flex justify-center items-center'>
-          <p className='text-white text-lg'>
-            Your mobile browser doesn't support 3D animation. Please visit the desktop version to experience the full website.
-          </p>
+      {!isMobile && (
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className='w-3 h-3 rounded-full bg-secondary mb-1'
+              />
+            </div>
+          </a>
         </div>
       )}
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
